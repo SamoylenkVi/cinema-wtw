e3e3d6a7efd34e4ba4e0f838730cf4460384cae1
Viktoriia Sevostianova <vikapasha14@gmail.com>
2023-05-17 17:43:23 +0700

    добавляет моки комментариев

---
+++ src/mocks/comments.js
@@ -0,0 +1,62 @@
+export const comments = [
+  {
+    id: 1,
+    user: {
+      id: 4,
+      name: 'Kate Muir',
+    },
+    rating: 8.9,
+    commentText: "Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.",
+    date: '2019-05-08T14:13:56.569Z',
+  },
+  {
+    id: 2,
+    user: {
+      id: 5,
+      name: 'Pasha Sevost',
+    },
+    rating: 7.1,
+    commentText: "Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. The Grand Budapest Hotel is no different, except that he has added a hint of gravitas to the mix, improving the recipe.",
+    date: '2020-06-09T14:13:56.569Z',
+  },
+  {
+    id: 3,
+    user: {
+      id: 6,
+      name: 'Vikaaaa',
+    },
+    rating: 9,
+    commentText: "I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.",
+    date: '2017-07-10T14:13:56.569Z',
+  },
+  {
+    id: 1,
+    user: {
+      id: 7,
+      name: 'Liudmila',
+    },
+    rating: 7.5,
+    commentText: 'The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.',
+    date: '2019-09-08T14:13:56.569Z',
+  },
+  {
+    id: 1,
+    user: {
+      id: 4,
+      name: 'Viks Twiks',
+    },
+    rating: 8.9,
+    commentText: 'It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+  {
+    id: 1,
+    user: {
+      id: 4,
+      name: 'Kate Muir',
+    },
+    rating: 8.9,
+    commentText: 'It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+];
